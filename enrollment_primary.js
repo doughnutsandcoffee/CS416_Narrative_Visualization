1ac78736d40b3d4f71d3fe44b1fd2c97235ca5fe
doughnutsandcoffee <140092810+doughnutsandcoffee@users.noreply.github.com>
2023-07-31 15:20:01 -0500

    Add files via upload

--- enrollment_primary.js
+++ enrollment_primary.js
@@ -105,7 +105,7 @@ function main() {
 
 //Get data: x and y axis use d3.extent() to set domain automatically, why inside d3.csv()
 d3.csv("all_years_pivot_edit.csv").then(function(data) {
-    
+
     //Select columns and filter out blank data
     let selectedData = data.map(function(d) {
         if (d.Region !== "" && d.Country !== "" && d.Income !== "" && d.GovtExpEd !== "" && d.EnrollmentPrimary !== "") {
@@ -185,21 +185,17 @@ d3.csv("all_years_pivot_edit.csv").then(function(data) {
 
     //GPI parity - horizontal line and label
     svg.append("line")
-        .attr("x1", 0)
-        .attr("y1", y(1))
-        .attr("x2", width)
-        .attr("y2", y(1))
+        .attr("class", "parity_line")
         .attr("stroke-width", 1)
         .attr("stroke", "black")
         .style("stroke-dasharray", ("3, 3"))
+        .style("visibility", "hidden");
 
     svg.append("text")
         .attr("class", "parity_label")
         .attr("text-anchor", "middle")
-        .attr("x", width / 2)
-        .attr("y", y(1))
-        .text("gender parity, (GPI) = 1")
         .style("fill", "grey")
+        .style("visibility", "hidden");
 
     //Scatter plot + Tooltip + Click to filter by Income|Region  
     const dots = svg.append("g")
@@ -279,6 +275,21 @@ d3.csv("all_years_pivot_edit.csv").then(function(data) {
         .duration(4900)
         .style("visibility", "visible")
         .call(d3.axisLeft(y))
+        .on("end", function() {
+            //GPI parity - horizontal line and label
+            svg.select(".parity_line")
+                .style("visibility", "visible")
+                .attr("x1", 0)
+                .attr("y1", y(1))
+                .attr("x2", width)
+                .attr("y2", y(1));
+
+            svg.select(".parity_label")
+                .style("visibility", "visible")
+                .attr("x", width / 2)
+                .attr("y", y(1))
+                .text("gender parity, (GPI) = 1");
+        });
 
     //Highlight dots
     dots.transition()
